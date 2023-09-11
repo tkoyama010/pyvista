9c678a812df1127e3314b0195939c41122f4e8d1
Tetsuo Koyama <tkoyama010@gmail.com>
2023-09-12 04:49:53 +0900

    Fix locale setting

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 # Otherwise VTK reader issues on some systems, causing sphinx to crash. See also #226.
-locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+locale.setlocale(locale.LC_ALL, "")
 
 faulthandler.enable()
 
